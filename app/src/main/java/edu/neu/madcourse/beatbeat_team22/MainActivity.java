a9be75af3aac95c37e56ba0109b7fa6e60a48602
maanasanarayan <mnsnryn@gmail.com>
2022-04-06 17:22:40 -0400

    Firebase integration.

--- app/src/main/java/edu/neu/madcourse/beatbeat_team22/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/beatbeat_team22/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/*
+Shows title activity. If user has logged in before on the same device, proceeds to the
+Home Page activity. Otherwise, proceeds to the Login activity.
+ */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Call signup activity
     }
 }
